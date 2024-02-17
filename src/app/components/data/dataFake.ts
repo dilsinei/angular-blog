74bdeb1e1180fe8ee1024dd00c610df59b1375df
dilsinei <dilsinei@gmail.com>
2024-02-17 10:48:19 -0300

    content components dynamics

---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,36 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu a nova versão do Angular",
+    "photo": "https://media.licdn.com/dms/image/D4D12AQFOdgCZsbM1ew/article-cover_image-shrink_600_2000/0/1688956655167?e=2147483647&v=beta&t=lgoW0GRE47Xzs7ZAnV5o6Aj-UaD1Rf3baE7HmNElq8Q",
+    "description": "A versão mais recente do Angular é a 17, lançada em 6 de novembro de 2023. Essa versão é considerada um renascimento do framework, com foco em melhorar a experiência do desenvolvedor e a performance das aplicações."
+  },
+
+  {
+    "id": "2",
+    "title": "Vagas de ti devem aumentar em 2025",
+    "photo": "https://jcconcursos.com.br/media/_versions/noticia/vagas-abertas-rgb-azul_widelg.png",
+    "description": "De acordo com um relatório publicado pela International Data Corporation (IDC) sobre o panorama do setor de Tecnologia da Informação (TI) para 2024, a indústria do Brasil tem previsão de crescer 12%, enquanto nos Estados crescerá 9% este ano."
+  },
+  {
+    "id": "3",
+    "title": "Qual será o futuro da linguagem JavaScript",
+    "photo":"https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg",
+    "description": " O futuro da linguagem JavaScript é promissor e cheio de oportunidades para desenvolvedores. Diversos fatores contribuem para essa perspectiva otimista. Em resumo, o futuro da linguagem JavaScript é sólido e cheio de oportunidades para quem deseja se destacar no mercado de desenvolvimento. Aprender JavaScript agora é um investimento com grande retorno no futuro."
+  },
+  {
+    "id": "4",
+    "title": "Será que a AI irá substituir o programador?",
+    "photo": "https://futurodasaude.com.br/wp-content/uploads/2023/07/Inteligencia-artificial-generativa-Futuro-Explica.jpeg",
+    "description":"A IA é uma ferramenta poderosa que pode auxiliar os programadores, mas não irá substituí-los completamente. Os programadores que se adaptarem às novas tecnologias e desenvolverem as habilidades necessárias para trabalhar com IA serão os mais bem-sucedidos no futuro.",
+
+  },
+
+  {
+    "id": "5",
+    "title": "Viúva Negra",
+    "photo": "https://s.aficionados.com.br/imagens/descubra-os-segredos-da-viuva-negra-a-espia-da-marvel_t.jpg",
+    "description": " Atualmente esta personagem está bastante associada aos Vingadores e com os super-heróis graças aos filmes da Marvel. Contudo, nem sempre as coisas foram assim.Viúva Negra possui um passado mais sombrio do que aparenta.Confira abaixo a história dessa incrível espiã."
+  },
+
+]
